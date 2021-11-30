de96362a377bfda7b3818dd43bbd46d6b7d576e1
Erik Jagyugya <jagyugyaerik@gmail.com>
2021-11-30 10:15:04 +0100

    Add new workflow
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resourge_group" "this" {
+resource "azurerm_resource_group" "this" {
   name     = "rg-action-test"
   location = "westeurope"
 }

ef7f032e904761c6c432f0cd35c4f75ccfeeaeed
Erik Jagyugya <jagyugyaerik@gmail.com>
2021-12-01 01:50:40 +0100

    Pass file for pwsh
    Signed-off-by: Erik Jagyugya <jagyugyaerik@gmail.com>

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "null_resource" "delete_tags" {
-#   depends_on = [
-#     azuread_application.app,
-#     azuread_service_principal.app,
-#     azuread_group.group
-#   ]
+  #   depends_on = [
+  #     azuread_application.app,
+  #     azuread_service_principal.app,
+  #     azuread_group.group
+  #   ]
 
-#   triggers = {
-#     resource_group_name = module.caf.resource_groups[local.default_resource_group_key].name
-#   }
+  #   triggers = {
+  #     resource_group_name = module.caf.resource_groups[local.default_resource_group_key].name
+  #   }
 
   provisioner "local-exec" {
     command     = format("%s/scripts/HelloWorld.ps1", path.module)
